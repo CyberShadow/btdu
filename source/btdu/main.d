CyberShadow/btdu

commit 1f1fbd47049429a8b0912a9445bf6679323e5a3f

btdu.subproc: Limit number of iterations in Subprocess.handleInput
Fixes #12.

source/btdu/main.d
@@ -167,6 +167,10 @@ Please report defects and enhancement requests to the GitHub issue tracker:
 	enum refreshInterval = 500.msecs;
 	auto nextRefresh = startTime;
 
+	enum totalMaxDuration = 1.seconds / 60; // 60 FPS
+	// How much time we may spend processing one subprocess's output:
+	auto subprocMaxDuration = totalMaxDuration / subprocesses.length;
+
 	auto readSet = new SocketSet;
 	auto exceptSet = new SocketSet;
 
@@ -207,7 +211,7 @@ Please report defects and enhancement requests to the GitHub issue tracker:
 		if (!paused)
 			foreach (ref subproc; subprocesses)
 				if (readSet.isSet(subproc.socket))
-					subproc.handleInput();
+					subproc.handleInput(subprocMaxDuration);
 		if (!headless && now > nextRefresh)
 		{
 			browser.update();

source/btdu/subproc.d
@@ -21,6 +21,7 @@ module btdu.subproc;
 
 import core.sys.posix.signal;
 import core.sys.posix.unistd;
+import core.time;
 
 import std.algorithm.iteration;
 import std.algorithm.mutation;
@@ -80,9 +81,11 @@ struct Subprocess
 	private size_t bufStart, bufEnd;
 
 	/// Called when select() identifies that the process wrote something.
-	void handleInput()
+	void handleInput(Duration maxDuration)
 	{
-		while (true)
+		auto deadline = MonoTime.currTime() + maxDuration;
+		// Ensure the loop exits eventually, even when there is always more data to process:
+		do
 		{
 			auto data = buf[bufStart .. bufEnd];
 			auto bytesNeeded = parse(data, this);
@@ -114,6 +117,7 @@ struct Subprocess
 			}
 			bufEnd += received;
 		}
+		while (MonoTime.currTime() < deadline);
 	}
 
 	void handleMessage(StartMessage m)

commit e977b60901476616392e808bdb0092cab2944c38

btdu.main: Fix --import
Broke in 1f1fbd47049429a8b0912a9445bf6679323e5a3f.

source/btdu/main.d
@@ -173,7 +173,7 @@ Please report defects and enhancement requests to the GitHub issue tracker:
 
 	enum totalMaxDuration = 1.seconds / 60; // 60 FPS
 	// How much time we may spend processing one subprocess's output:
-	auto subprocMaxDuration = totalMaxDuration / subprocesses.length;
+	auto subprocMaxDuration = totalMaxDuration / (1 + subprocesses.length);
 
 	auto readSet = new SocketSet;
 	auto exceptSet = new SocketSet;