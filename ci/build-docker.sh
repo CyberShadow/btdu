c6ae8ae641c2ef58388bd1993516e58588a7eec1
Vladimir Panteleev <git@cy.md>
2022-10-31 12:55:17 +0000

    ci/build-docker.sh: Allow customizing the container runtime
    You can thus use Podman to make a release build of btdu with:
    
    DOCKER=podman ci/build-docker.sh

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -3,6 +3,8 @@ set -eEuo pipefail
 
 cd "$(dirname "$0")"
 
+docker=${DOCKER-docker}
+
 arches=("$@")
 if [[ ${#arches[@]} -eq 0 ]]
 then
@@ -12,6 +14,6 @@ fi
 
 for arch in "${arches[@]}"
 do
-	docker build --build-arg BTDU_ARCH="$arch" -t btdu-"$arch" docker
-	docker run --rm -v "$(cd .. && pwd)":/btdu --env BTDU_ARCH="$arch" btdu-"$arch" /btdu/ci/build-inside-docker.sh
+	"$docker" build --build-arg BTDU_ARCH="$arch" -t btdu-"$arch" docker
+	"$docker" run --rm -v "$(cd .. && pwd)":/btdu --env BTDU_ARCH="$arch" btdu-"$arch" /btdu/ci/build-inside-docker.sh
 done
