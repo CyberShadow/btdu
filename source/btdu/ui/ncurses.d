e5b6c6cd2231a0e0d247128b9d0d7cffa76ae548
Vladimir Panteleev <git@cy.md>
2026-01-03 21:51:40 +0000

    [WIP] btdu.ui.ncurses: Remove redundant shims for macros ImportC handles directly
    - Remove D aliases and C enum exports for KEY_*, OK, ERR, CCHARW_MAX,
      and KEY_F() since ImportC can evaluate these macros directly
    - Keep only A_* attribute shims which use NCURSES_BITS macro containing
      a (chtype) cast that ImportC cannot evaluate
    - Move stdscr wrapper property from C inline function to D code
    - Update comments to clarify which macros need special handling

--- source/btdu/ui/ncurses.d
+++ source/btdu/ui/ncurses.d
@@ -3,12 +3,22 @@
 module btdu.ui.ncurses;
 
 // Import the C shim - all ncurses declarations become available
+// Most ncurses macros (KEY_*, OK, ERR, CCHARW_MAX, KEY_F()) work directly.
+// Only A_* attributes need aliasing due to (chtype) casts ImportC can't evaluate.
 public import btdu_ncurses_importc;
 
 // Re-export wchar_t type for compatibility with existing D code
 public import core.stdc.stddef : wchar_t;
 
+// Wrapper for stdscr - on reentrant ncurses builds (NCURSES_REENTRANT=1),
+// stdscr is a macro that expands to a function call which ImportC doesn't export
+@property WINDOW* stdscr() @trusted
+{
+    return _nc_get_stdscr();
+}
+
 // Attributes - alias internal names to standard ncurses names
+// These need shims because they use NCURSES_BITS macro with (chtype) cast
 alias A_NORMAL = _NC_A_NORMAL;
 alias A_STANDOUT = _NC_A_STANDOUT;
 alias A_UNDERLINE = _NC_A_UNDERLINE;
@@ -26,124 +36,6 @@ alias A_RIGHT = _NC_A_RIGHT;
 alias A_TOP = _NC_A_TOP;
 alias A_VERTICAL = _NC_A_VERTICAL;
 alias A_ITALIC = _NC_A_ITALIC;
-
-// Return codes
-alias OK = _NC_OK;
-alias ERR = _NC_ERR;
-
-// Configuration
-alias CCHARW_MAX = _NC_CCHARW_MAX;
-
-// KEY_F as a CTFE function (matches ncurses KEY_F(n) macro calling convention)
-int KEY_F(int n) pure nothrow @nogc @safe
-{
-    return _NC_KEY_F0 + n;
-}
-
-// Basic navigation keys
-alias KEY_DOWN = _NC_KEY_DOWN;
-alias KEY_UP = _NC_KEY_UP;
-alias KEY_LEFT = _NC_KEY_LEFT;
-alias KEY_RIGHT = _NC_KEY_RIGHT;
-alias KEY_HOME = _NC_KEY_HOME;
-alias KEY_BACKSPACE = _NC_KEY_BACKSPACE;
-alias KEY_DL = _NC_KEY_DL;
-alias KEY_IL = _NC_KEY_IL;
-alias KEY_DC = _NC_KEY_DC;
-alias KEY_IC = _NC_KEY_IC;
-alias KEY_EIC = _NC_KEY_EIC;
-alias KEY_CLEAR = _NC_KEY_CLEAR;
-alias KEY_EOS = _NC_KEY_EOS;
-alias KEY_EOL = _NC_KEY_EOL;
-alias KEY_SF = _NC_KEY_SF;
-alias KEY_SR = _NC_KEY_SR;
-alias KEY_NPAGE = _NC_KEY_NPAGE;
-alias KEY_PPAGE = _NC_KEY_PPAGE;
-alias KEY_STAB = _NC_KEY_STAB;
-alias KEY_CTAB = _NC_KEY_CTAB;
-alias KEY_CATAB = _NC_KEY_CATAB;
-alias KEY_ENTER = _NC_KEY_ENTER;
-alias KEY_PRINT = _NC_KEY_PRINT;
-alias KEY_LL = _NC_KEY_LL;
-alias KEY_A1 = _NC_KEY_A1;
-alias KEY_A3 = _NC_KEY_A3;
-alias KEY_B2 = _NC_KEY_B2;
-alias KEY_C1 = _NC_KEY_C1;
-alias KEY_C3 = _NC_KEY_C3;
-alias KEY_BTAB = _NC_KEY_BTAB;
-alias KEY_BEG = _NC_KEY_BEG;
-alias KEY_CANCEL = _NC_KEY_CANCEL;
-alias KEY_CLOSE = _NC_KEY_CLOSE;
-alias KEY_COMMAND = _NC_KEY_COMMAND;
-alias KEY_COPY = _NC_KEY_COPY;
-alias KEY_CREATE = _NC_KEY_CREATE;
-alias KEY_END = _NC_KEY_END;
-alias KEY_EXIT = _NC_KEY_EXIT;
-alias KEY_FIND = _NC_KEY_FIND;
-alias KEY_HELP = _NC_KEY_HELP;
-alias KEY_MARK = _NC_KEY_MARK;
-alias KEY_MESSAGE = _NC_KEY_MESSAGE;
-alias KEY_MOVE = _NC_KEY_MOVE;
-alias KEY_NEXT = _NC_KEY_NEXT;
-alias KEY_OPEN = _NC_KEY_OPEN;
-alias KEY_OPTIONS = _NC_KEY_OPTIONS;
-alias KEY_PREVIOUS = _NC_KEY_PREVIOUS;
-alias KEY_REDO = _NC_KEY_REDO;
-alias KEY_REFERENCE = _NC_KEY_REFERENCE;
-alias KEY_REFRESH = _NC_KEY_REFRESH;
-alias KEY_REPLACE = _NC_KEY_REPLACE;
-alias KEY_RESTART = _NC_KEY_RESTART;
-alias KEY_RESUME = _NC_KEY_RESUME;
-alias KEY_SAVE = _NC_KEY_SAVE;
-alias KEY_SUSPEND = _NC_KEY_SUSPEND;
-alias KEY_UNDO = _NC_KEY_UNDO;
-alias KEY_MOUSE = _NC_KEY_MOUSE;
-
-// Shifted keys
-alias KEY_SBEG = _NC_KEY_SBEG;
-alias KEY_SCANCEL = _NC_KEY_SCANCEL;
-alias KEY_SCOMMAND = _NC_KEY_SCOMMAND;
-alias KEY_SCOPY = _NC_KEY_SCOPY;
-alias KEY_SCREATE = _NC_KEY_SCREATE;
-alias KEY_SDC = _NC_KEY_SDC;
-alias KEY_SDL = _NC_KEY_SDL;
-alias KEY_SELECT = _NC_KEY_SELECT;
-alias KEY_SEND = _NC_KEY_SEND;
-alias KEY_SEOL = _NC_KEY_SEOL;
-alias KEY_SEXIT = _NC_KEY_SEXIT;
-alias KEY_SFIND = _NC_KEY_SFIND;
-alias KEY_SHELP = _NC_KEY_SHELP;
-alias KEY_SHOME = _NC_KEY_SHOME;
-alias KEY_SIC = _NC_KEY_SIC;
-alias KEY_SLEFT = _NC_KEY_SLEFT;
-alias KEY_SMESSAGE = _NC_KEY_SMESSAGE;
-alias KEY_SMOVE = _NC_KEY_SMOVE;
-alias KEY_SNEXT = _NC_KEY_SNEXT;
-alias KEY_SOPTIONS = _NC_KEY_SOPTIONS;
-alias KEY_SPREVIOUS = _NC_KEY_SPREVIOUS;
-alias KEY_SPRINT = _NC_KEY_SPRINT;
-alias KEY_SREDO = _NC_KEY_SREDO;
-alias KEY_SREPLACE = _NC_KEY_SREPLACE;
-alias KEY_SRIGHT = _NC_KEY_SRIGHT;
-alias KEY_SRSUME = _NC_KEY_SRSUME;
-alias KEY_SSAVE = _NC_KEY_SSAVE;
-alias KEY_SSUSPEND = _NC_KEY_SSUSPEND;
-alias KEY_SUNDO = _NC_KEY_SUNDO;
-
-// Function key aliases for convenience
-alias KEY_F0 = _NC_KEY_F0;
-alias KEY_F1 = _NC_KEY_F1;
-alias KEY_F2 = _NC_KEY_F2;
-alias KEY_F3 = _NC_KEY_F3;
-alias KEY_F4 = _NC_KEY_F4;
-alias KEY_F5 = _NC_KEY_F5;
-alias KEY_F6 = _NC_KEY_F6;
-alias KEY_F7 = _NC_KEY_F7;
-alias KEY_F8 = _NC_KEY_F8;
-alias KEY_F9 = _NC_KEY_F9;
-alias KEY_F10 = _NC_KEY_F10;
-alias KEY_F11 = _NC_KEY_F11;
-alias KEY_F12 = _NC_KEY_F12;
 
 // NCURSES_PAIRS_T is a macro that expands to 'short'
 alias NCURSES_PAIRS_T = short;
--- source/btdu_ncurses_importc.c
+++ source/btdu_ncurses_importc.c
@@ -2,19 +2,16 @@
 // Captures system ncurses configuration (NCURSES_OPAQUE, etc.) automatically.
 //
 // This file is compiled by ImportC to extract ncurses declarations.
-// C enums export macro constants as D manifest constants.
-//
-// Note: dub.sdl passes -P-std=c11 to force C11 mode, which ensures
-// stdbool.h defines bool as _Bool (GCC 15+ defaults to C23 where bool
-// is a keyword that ImportC doesn't implement). This workaround can be
-// removed after requiring DMD >= 2.112.0 (adds -std=c11 automatically).
+// Most ncurses macros are directly accessible via ImportC, but some
+// (like A_BOLD) contain casts to typedef types that ImportC can't evaluate.
+// We export those as C enums which become D manifest constants.
 
 // Disable glibc inline functions that cause infinite recursion with ImportC.
 // These inlines (like wctob) shadow libc functions and call themselves.
 // __NO_INLINE__ prevents features.h from defining __USE_EXTERN_INLINES.
 #define __NO_INLINE__ 1
 
-// Feature test macros - pkg-config recommends these for ncursesw
+// Feature test macros - enables wide character support (NCURSES_WIDECHAR=1)
 #define _DEFAULT_SOURCE 1
 #define _XOPEN_SOURCE 600
 #define _XOPEN_SOURCE_EXTENDED 1
@@ -25,32 +22,18 @@
 // Try ncursesw first (wide character support), fall back to ncurses
 // Note: dub's "libs" uses pkg-config only for linking, not for C preprocessing,
 // so we use __has_include to find the correct header path
-// TODO: Fix dub to pass pkg-config cflags to ImportC preprocessor
 #if __has_include(<ncursesw/ncurses.h>)
 #include <ncursesw/ncurses.h>
 #else
 #include <ncurses.h>
 #endif
 
-// Wrapper for stdscr - on reentrant ncurses builds, stdscr is a macro
-// that expands to a function call which ImportC may not handle correctly
+// Wrapper for stdscr - on reentrant ncurses builds (NCURSES_REENTRANT=1),
+// stdscr is a macro that expands to a function call which ImportC doesn't export
 static inline WINDOW* _nc_get_stdscr(void) { return stdscr; }
 
-// Export macro constants as C enums (become D manifest constants)
-// Using _NC_ prefix; D wrapper aliases these to original names
-
-// Return codes - separate enum to handle negative ERR value
-enum {
-    _NC_OK = 0,
-    _NC_ERR = -1,
-};
-
-// Configuration
-enum {
-    _NC_CCHARW_MAX = CCHARW_MAX,
-};
-
-// Attributes
+// Attributes - these macros use NCURSES_BITS which contains a (chtype) cast
+// that ImportC can't evaluate, so we export them as enum constants
 enum {
     _NC_A_NORMAL = A_NORMAL,
     _NC_A_STANDOUT = A_STANDOUT,
@@ -69,111 +52,4 @@ enum {
     _NC_A_TOP = A_TOP,
     _NC_A_VERTICAL = A_VERTICAL,
     _NC_A_ITALIC = A_ITALIC,
-};
-
-// Key codes
-enum {
-    _NC_KEY_DOWN = KEY_DOWN,
-    _NC_KEY_UP = KEY_UP,
-    _NC_KEY_LEFT = KEY_LEFT,
-    _NC_KEY_RIGHT = KEY_RIGHT,
-    _NC_KEY_HOME = KEY_HOME,
-    _NC_KEY_BACKSPACE = KEY_BACKSPACE,
-    _NC_KEY_DL = KEY_DL,
-    _NC_KEY_IL = KEY_IL,
-    _NC_KEY_DC = KEY_DC,
-    _NC_KEY_IC = KEY_IC,
-    _NC_KEY_EIC = KEY_EIC,
-    _NC_KEY_CLEAR = KEY_CLEAR,
-    _NC_KEY_EOS = KEY_EOS,
-    _NC_KEY_EOL = KEY_EOL,
-    _NC_KEY_SF = KEY_SF,
-    _NC_KEY_SR = KEY_SR,
-    _NC_KEY_NPAGE = KEY_NPAGE,
-    _NC_KEY_PPAGE = KEY_PPAGE,
-    _NC_KEY_STAB = KEY_STAB,
-    _NC_KEY_CTAB = KEY_CTAB,
-    _NC_KEY_CATAB = KEY_CATAB,
-    _NC_KEY_ENTER = KEY_ENTER,
-    _NC_KEY_PRINT = KEY_PRINT,
-    _NC_KEY_LL = KEY_LL,
-    _NC_KEY_A1 = KEY_A1,
-    _NC_KEY_A3 = KEY_A3,
-    _NC_KEY_B2 = KEY_B2,
-    _NC_KEY_C1 = KEY_C1,
-    _NC_KEY_C3 = KEY_C3,
-    _NC_KEY_BTAB = KEY_BTAB,
-    _NC_KEY_BEG = KEY_BEG,
-    _NC_KEY_CANCEL = KEY_CANCEL,
-    _NC_KEY_CLOSE = KEY_CLOSE,
-    _NC_KEY_COMMAND = KEY_COMMAND,
-    _NC_KEY_COPY = KEY_COPY,
-    _NC_KEY_CREATE = KEY_CREATE,
-    _NC_KEY_END = KEY_END,
-    _NC_KEY_EXIT = KEY_EXIT,
-    _NC_KEY_FIND = KEY_FIND,
-    _NC_KEY_HELP = KEY_HELP,
-    _NC_KEY_MARK = KEY_MARK,
-    _NC_KEY_MESSAGE = KEY_MESSAGE,
-    _NC_KEY_MOVE = KEY_MOVE,
-    _NC_KEY_NEXT = KEY_NEXT,
-    _NC_KEY_OPEN = KEY_OPEN,
-    _NC_KEY_OPTIONS = KEY_OPTIONS,
-    _NC_KEY_PREVIOUS = KEY_PREVIOUS,
-    _NC_KEY_REDO = KEY_REDO,
-    _NC_KEY_REFERENCE = KEY_REFERENCE,
-    _NC_KEY_REFRESH = KEY_REFRESH,
-    _NC_KEY_REPLACE = KEY_REPLACE,
-    _NC_KEY_RESTART = KEY_RESTART,
-    _NC_KEY_RESUME = KEY_RESUME,
-    _NC_KEY_SAVE = KEY_SAVE,
-    _NC_KEY_SUSPEND = KEY_SUSPEND,
-    _NC_KEY_UNDO = KEY_UNDO,
-    _NC_KEY_MOUSE = KEY_MOUSE,
-
-    // Shifted keys
-    _NC_KEY_SBEG = KEY_SBEG,
-    _NC_KEY_SCANCEL = KEY_SCANCEL,
-    _NC_KEY_SCOMMAND = KEY_SCOMMAND,
-    _NC_KEY_SCOPY = KEY_SCOPY,
-    _NC_KEY_SCREATE = KEY_SCREATE,
-    _NC_KEY_SDC = KEY_SDC,
-    _NC_KEY_SDL = KEY_SDL,
-    _NC_KEY_SELECT = KEY_SELECT,
-    _NC_KEY_SEND = KEY_SEND,
-    _NC_KEY_SEOL = KEY_SEOL,
-    _NC_KEY_SEXIT = KEY_SEXIT,
-    _NC_KEY_SFIND = KEY_SFIND,
-    _NC_KEY_SHELP = KEY_SHELP,
-    _NC_KEY_SHOME = KEY_SHOME,
-    _NC_KEY_SIC = KEY_SIC,
-    _NC_KEY_SLEFT = KEY_SLEFT,
-    _NC_KEY_SMESSAGE = KEY_SMESSAGE,
-    _NC_KEY_SMOVE = KEY_SMOVE,
-    _NC_KEY_SNEXT = KEY_SNEXT,
-    _NC_KEY_SOPTIONS = KEY_SOPTIONS,
-    _NC_KEY_SPREVIOUS = KEY_SPREVIOUS,
-    _NC_KEY_SPRINT = KEY_SPRINT,
-    _NC_KEY_SREDO = KEY_SREDO,
-    _NC_KEY_SREPLACE = KEY_SREPLACE,
-    _NC_KEY_SRIGHT = KEY_SRIGHT,
-    _NC_KEY_SRSUME = KEY_SRSUME,
-    _NC_KEY_SSAVE = KEY_SSAVE,
-    _NC_KEY_SSUSPEND = KEY_SSUSPEND,
-    _NC_KEY_SUNDO = KEY_SUNDO,
-
-    // Function keys - KEY_F(n) macro
-    _NC_KEY_F0 = KEY_F(0),
-    _NC_KEY_F1 = KEY_F(1),
-    _NC_KEY_F2 = KEY_F(2),
-    _NC_KEY_F3 = KEY_F(3),
-    _NC_KEY_F4 = KEY_F(4),
-    _NC_KEY_F5 = KEY_F(5),
-    _NC_KEY_F6 = KEY_F(6),
-    _NC_KEY_F7 = KEY_F(7),
-    _NC_KEY_F8 = KEY_F(8),
-    _NC_KEY_F9 = KEY_F(9),
-    _NC_KEY_F10 = KEY_F(10),
-    _NC_KEY_F11 = KEY_F(11),
-    _NC_KEY_F12 = KEY_F(12),
 };
